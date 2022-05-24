db57277d2e3ca34f33abaf49b8c8a7d9fc9f48d8
Raquel Lais de Souza <rls.lic18@uea.edu.br>
2022-05-23 22:16:12 -0300

    campoPesquisa

--- index.py
+++ index.py
@@ -1,17 +1,14 @@
 from pywebio.output import *
+from pywebio import start_server
+from pywebio.input import *
 
-put_image(open('logoSC.png','rb').read());
+put_image(open('logoSC.png', 'rb').read());
 
-put_text('Precisando daquela sugestão do chefe? Encontre as melhores receitas aqui e aprovete!').style('color: black; font-size:25px; margin-top:30px; margin-bottom:30px');
+put_text('Precisando daquela sugestão do chefe? Encontre as melhores receitas aqui e aprovete!').style(
+    'color: black; font-size:25px; margin-top:30px; margin-bottom:30px');
 
-put_table([
-    ['TypeTypeTypeTypeTypeTypeTypeTypeTypeTypeTypeTypeTypeType', 'Content'],
-    ['html', put_html('X<sup>2</sup>')],
-    ['text', '<hr/>'],  # equal to ['text', put_text('<hr/>')]
-    ['buttons', put_buttons(['A', 'B'], onclick=...)],
-    ['markdown', put_markdown('`Awesome PyWebIO!`')],
-    ['file', put_file('hello.text', b'hello world')],
-    ['table', put_table([['A', 'B'], ['C', 'D']])]
-])
+
+text = textarea('Pesquisar receita', rows=3, placeholder='Some text')
+put_text('text = %r' % text)
 
 
