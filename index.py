a31c4c313de679cfcfad5846d74b8d23ab00942e
Raquel Lais de Souza <rls.lic18@uea.edu.br>
2022-05-24 00:37:09 -0300

    update imagem e tabela

--- index.py
+++ index.py
@@ -1,12 +1,34 @@
-from pywebio.output import *
 from pywebio import start_server
 from pywebio.input import *
+from pywebio.output import *
+from pywebio.session import *
+from pywebio.pin import *
 
-put_image(open('logoSC.png', 'rb').read());
+def main():
+    put_image(open('logoSC.png', 'rb').read());
 
-put_text('Precisando daquela sugestão do chefe? Encontre as melhores receitas aqui e aprovete!').style(
-    'color: black; font-size:25px; margin-top:30px; margin-bottom:30px');
+    put_text('Precisando daquela sugestão do chefe? Encontre as melhores receitas aqui e aprovete!').style(
+        'color: #025D00; font-size:25px; margin-top:25px; margin-bottom:30px');
+
+    text = textarea('Pesquisar receita', rows=3, placeholder='Nome da Receita')
 
 
+    put_row([
+        put_column([
+            put_row([
+                put_table([['Nome receita', 'Ingredientes'], [put_text('%r' % text), 'D']]),
+                put_image(open('chefLogo.png', 'rb').read())
+,
+            ]),
+        ]),
+
+    ])
+
+<<<<<<< Updated upstream
 text = textarea('Pesquisar receita', rows=3, placeholder='Nome da Receita')
 put_text('text = %r' % text)
+=======
+
+
+start_server(main, port=8080, debug=True)
+>>>>>>> Stashed changes
